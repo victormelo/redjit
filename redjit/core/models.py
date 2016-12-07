ae23bacc132250e00000978bef47f12c545ecc4c
victormelo <victorklbr@gmail.com>
2016-12-07 15:04:07 -0300

    initial model structure

--- redjit/core/models.py
+++ redjit/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from vote.managers import VotableManager
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Link(models.Model):
+    url = models.CharField(max_length=50)
+    votes = VotableManager()
+    user = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return self.name
+
+class Comment(models.Model):
+    text = models.TextField()
+    link = models.ForeignKey(Link)
+    user = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return '%s on %s' % (self.user, self.link.id)
